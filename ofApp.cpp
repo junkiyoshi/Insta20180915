8a3e5c8ff031ebedb9ed95d72a671baf13be697a
nakauchi <junkiyoshi@gmail.com>
2018-09-15 19:04:57 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,55 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openframeworks");
+
+	this->cap.open(1);
+	this->cap_size = cv::Size(1280, 720);
+
+	this->image.allocate(this->cap_size.width, this->cap_size.height, OF_IMAGE_COLOR);
+	this->frame = cv::Mat(cv::Size(this->image.getWidth(), this->image.getHeight()), CV_MAKETYPE(CV_8UC3, this->image.getPixels().getNumChannels()), this->image.getPixels().getData(), 0);
+
+	this->shader.load("shader/shader.vert", "shader/shader.frag");
+}
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	cv::Mat src;
+	this->cap >> src;
+	if (src.empty()) {
+
+		// If capture source is video
+		this->cap.set(CV_CAP_PROP_POS_FRAMES, 1);
+		return;
+	}
+	cv::resize(src, this->frame, this->cap_size);
+	cv::cvtColor(this->frame, this->frame, CV_BGR2RGB);
+	cv::flip(this->frame, this->frame, 2);
+	this->image.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->shader.begin();
+	this->shader.setUniform1f("time", ofGetElapsedTimef());
+	this->shader.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
+	this->shader.setUniformTexture("tex", this->image, 1);
+
+	ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+
+	this->shader.end();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3, 2);
+	settings.setSize(1280, 720);
+	ofCreateWindow(settings);
+	ofRunApp(new ofApp());
+}
